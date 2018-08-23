3614a3066b7597601cdc76b9d59258473db65e64
Tee <tee_tu845@hotmail.com>
2018-08-23 21:27:26 +0700

    test app

--- example/views.py
+++ example/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here
-def homeview(request)
-    return HttpResponse('hellow world')
+#def homeview(request)
+#    return HttpResponse('hellow world')
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
